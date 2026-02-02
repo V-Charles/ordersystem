b9d1ea34cdd7fbb432e79c3ad6be4b6905b2cc97
Vinicius Charles <viniciuscharles3101@gmail.com>
2026-02-02 20:03:02 -0300

    feat: Exception handling - delete

--- src/main/java/com/charles/vinicius/ordersystem/services/UserService.java
+++ src/main/java/com/charles/vinicius/ordersystem/services/UserService.java
@@ -2,8 +2,11 @@ package com.charles.vinicius.ordersystem.services;
 
 import com.charles.vinicius.ordersystem.entities.User;
 import com.charles.vinicius.ordersystem.repositories.UserRepository;
+import com.charles.vinicius.ordersystem.services.exceptions.DatabaseException;
 import com.charles.vinicius.ordersystem.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,14 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+         } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+
     }
 
     public User update(Long id, User obj) {
