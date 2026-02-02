88d2933aeee17da9b3021451e8df68192488a725
Vinicius Charles <viniciuscharles3101@gmail.com>
2026-02-02 19:27:18 -0300

    feat: Exception handling - findById

--- src/main/java/com/charles/vinicius/ordersystem/services/UserService.java
+++ src/main/java/com/charles/vinicius/ordersystem/services/UserService.java
@@ -2,6 +2,7 @@ package com.charles.vinicius.ordersystem.services;
 
 import com.charles.vinicius.ordersystem.entities.User;
 import com.charles.vinicius.ordersystem.repositories.UserRepository;
+import com.charles.vinicius.ordersystem.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
---
+++ src/main/java/com/charles/vinicius/ordersystem/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.charles.vinicius.ordersystem.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
